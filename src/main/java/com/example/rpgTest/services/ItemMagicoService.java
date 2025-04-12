e9556a348a180d6a26abf5c82f82c3f7af28d6d3
Hudson <hudson.um@gmail.com>
2025-04-12 17:36:16 -0300

    Adicionando as Services incompletas

---
+++ src/main/java/com/example/rpgTest/services/ItemMagicoService.java
@@ -0,0 +1,40 @@
+package com.example.rpgTest.services;
+
+import com.example.rpgTest.model.ItemMagicoModel;
+import com.example.rpgTest.repository.ItemMagicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemMagicoService {
+
+    @Autowired
+    private ItemMagicoRepository itemMagicoRepository;
+
+    public ResponseEntity<ItemMagicoModel> createItemMagico(@RequestBody ItemMagicoModel itemMagicoModel){
+        ItemMagicoModel novoItemMagico = itemMagicoRepository.save(itemMagicoModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoItemMagico);
+    }
+
+    public ResponseEntity<List<ItemMagicoModel>> getAllItemMagico(){
+        List<ItemMagicoModel> allItemMagico = itemMagicoRepository.findAll();
+        return ResponseEntity.ok(allItemMagico);
+    }
+
+    public ResponseEntity<Optional<ItemMagicoModel>> getItemMagicoById(@PathVariable Long id){
+        Optional<ItemMagicoModel> itemMagicoById = itemMagicoRepository.findById(id);
+        return ResponseEntity.ok(itemMagicoById);
+    }
+
+    public ResponseEntity<?> deleteItemMagicoById(@PathVariable Long id){
+        itemMagicoRepository.deleteById(id);
+        return ResponseEntity.ok("Deletado com Sucesso");
+    }
+}
---
+++ src/main/java/com/example/rpgTest/services/PersonagemService.java
@@ -0,0 +1,40 @@
+package com.example.rpgTest.services;
+
+import com.example.rpgTest.model.PersonagemModel;
+import com.example.rpgTest.repository.PersonagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonagemService {
+
+    @Autowired
+    private PersonagemRepository personagemRepository;
+
+    public ResponseEntity<PersonagemModel> createPersonagem(@RequestBody PersonagemModel personagemModel){
+        PersonagemModel novoPersonagem = personagemRepository.save(personagemModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoPersonagem);
+    }
+
+    public ResponseEntity<List<PersonagemModel>> getAllPersonagens(){
+        List<PersonagemModel> allPersonagens = personagemRepository.findAll();
+        return ResponseEntity.ok(allPersonagens);
+    }
+
+    public ResponseEntity<Optional<PersonagemModel>> getPersonagemById(@PathVariable Long id){
+        Optional<PersonagemModel> personagemById = personagemRepository.findById(id);
+        return ResponseEntity.ok(personagemById);
+    }
+
+    public ResponseEntity<?> deletePersonagemById(@PathVariable Long id){
+        personagemRepository.deleteById(id);
+        return ResponseEntity.ok("Personagem deletado com sucesso");
+    }
+}
